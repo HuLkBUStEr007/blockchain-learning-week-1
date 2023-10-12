e2f482caf159ec633da84916e2aa36976f31895e
AADARSH SINGH <95958161+HuLkBUStEr007@users.noreply.github.com>
2023-10-12 23:16:57 +0530

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -1,10 +1,10 @@
 for (let a = 1; a <101; a++){
-    if (a % 3 == 0){
-        console.log("fizz");
+    if (a % 15 == 0){
+        console.log("fizzbuzz");
     } else if (a % 5 == 0){
         console.log("buzz");
-    }else if (a % 3==0) && (a % 5 == 0){
-        console.log("fizzbuzz");
+    }else if (a % 3 == 0){
+        console.log("fizz");
     } else {
         console.log(a);
     }
